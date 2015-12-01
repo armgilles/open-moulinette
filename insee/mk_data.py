ee87de62935fba9f7c8e6ea4759bb86bd78aa2fa
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2015-12-01 22:36:03 +0100

    automatisation for revenu

--- insee/mk_data.py
+++ insee/mk_data.py
@@ -1,82 +1,26 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun  4 12:45:22 2015
-@author: Florian, Alexis
+
+Pour lire un fichier: on a besoin de plusieurs éléments: 
+    - le nom que l'on va utiliser dans le code
+    - l'url sur lequel on peut télécharger le zip
+    - le nom du que l'on veut utiliser pour le fichier excel
 """
 
 import os
 import pandas as pd
-from get_data import download_unzip
-
-path_insee = 'http://www.insee.fr/fr/ppp/bases-de-donnees/donnees-detaillees/'
-
-##################### IMPORT DES DONNEES COMMUNES ############################
-
-''' Import des données IRIS grâce à l'open-moulinette'''
-print("Initialisation...")
-
-path_data = 'data/'
-assert os.path.exists(path_data), 'il faut créer le fichier correspondant à path data'
-
-
-key = ['CODGEO', 'LIBGEO', 'COM', 'LIBCOM', 'REG', 'DEP', 'UU2010']
-
-columns_not_to_sum = ['CODGEO', 'LIBGEO', 'COM', 'LIBCOM', 'REG', 'DEP', 'ARR', 'CV',
-             'ZE2010', 'UU2010', 'UU12010', 'ID_MODIF_GEO']
-
-
-def insee_path_by_filename(filename):
-    url_path = path_insee + filename + '/' + filename + '-infra-13.zip'
-    return url_path
-
-
-def _read_iris_file(filename):
-    path_file_on_disk = path_data + filename + '-infra.xls'
-    if not os.path.exists(path_file_on_disk):
-        url_path = insee_path_by_filename(filename)
-        download_unzip(url_path, path_data)
-    return pd.read_excel(path_file_on_disk, sheetname='IRIS')
+from get_data import (read_equipement_file, change_headers,
+                      sum_of_all_features,
+                      revenu_url, _read_file_or_download, 
+                      path_data)
 
 
 
-def _change_headers(iris_df):
-    ''' iris_df is an iris data frame'''
-#    iris_df.rename(columns={'IRIS':'CODGEO'}, inplace=True)
-    header = iris_df.loc[4].tolist()
-    iris_df.columns = header
-    # to get real values
-    iris_df = iris_df[5:]
-    # clean Nan Columns
-    iris_df.dropna(how='any', axis=(1), inplace=True)
-    return iris_df
 
+##### équipements  #####
 
-def _sum_of_all_features(iris_df, colname, list_to_sum=None):
-    ''' iris_df is an iris data frame
-        colname is the name of the sum of columns'''
-    header = [x for x in iris_df.columns if str(x) != 'nan'] # remove NaN to header to create new sum feature
-    # creating new feature : sum of all feature
-    if list_to_sum is None:
-        features = [x for x in header if x not in columns_not_to_sum]
-    else:
-        features = list_to_sum
-    iris_df[colname] = iris_df[features].applymap(lambda x: float(x)).sum(axis=1)
-    nb_features = len(features)
-    print("\t il y a ", len(iris_df.CODGEO.unique()),
-          " iris différentes pour et ", nb_features, " features")
-    return iris_df
-
-
-def routine1(name):
-    assert name in iris_table
-    filename, list_to_sum = iris_table[name]
-    df = _read_iris_file(filename)
-    df = _change_headers(df)
-    df = _sum_of_all_features(df, 'nb_' + name, list_to_sum)
-
-    return df
-
-iris_table = dict(
+equipements = dict(
     commerce = ('equip-serv-commerce', None),
     sport = ('equip-sport-loisir-socio',
                       ['NB_F101', 'NB_F102', 'NB_F103', 'NB_F104',
@@ -100,84 +44,55 @@ iris_table = dict(
      transport_tourisme = ('equip-tour-transp', None),
      )
 
-
-data = None
-for table in iris_table:
+def routine1(name):
+    assert name in equipements
+    filename, list_to_sum = equipements[name]
+    df = read_equipement_file(filename)
+    df = change_headers(df, headers_line=4)
+    df = sum_of_all_features(df, 'nb_' + name, list_to_sum)
+    assert len(df) == df.CODGEO.nunique()
+    return df
+    
+equip_data = None
+for table in equipements:
     print('* lecture de ' + table)
-    if data is None:
-        data = routine1(table)
+    if equip_data is None:
+        equip_data = routine1(table)
     else:
-        data = data.merge(routine1(table), how='outer')
+        equip_data = equip_data.merge(routine1(table), how='outer')
 
 
-### Revenu have 4 files [ménage, personne, unité de consomation, ensemble]
-#-------------------------------------------------------------------------
+##### revenus  #####
 
-## Revenu Ménage
-revenu_menage = pd.read_excel(path_data + 'RFDM2011IRI.xls', sheetname=1) #using int cause name of sheetname have some "é"
-# creating header from file
-header = revenu_menage.loc[5].tolist()
-revenu_menage.columns = header
-revenu_menage.rename(columns={'IRIS':'CODGEO'}, inplace=True)
-# to get real values
-revenu_menage = revenu_menage[6:]
-# creating new feature : sum of all feature
-features = [x for x in header if x not in ['IRIS','LIBIRIS','COM','LIBCOM','REG','DEP','ARR','CV','ZE2010']] # special list for this file
-# No need to sum features here (% and quantile)
-features.append('CODGEO')
-print("il y a  %d iris différentes pour le revenu par ménage et %d features" % (len(revenu_menage.CODGEO.unique()), len(features) - 1))
+table_revenu = ['RFST', 'RFDM', 'RFDP', 'RFDU'] # l'ordre n'est pas 
+# aléatoire, c'est celui du fichier zip (ordre alphabétique) de 2011
+useless_cols = ['IRIS','LIBIRIS','COM','LIBCOM','REG','DEP','ARR','CV','ZE2010']
 
-data = pd.merge(data, revenu_menage[features], on='CODGEO', how='outer')
+def routine2(year):
+    url_path = revenu_url(year)
+    names = [x + str(year) + 'IRI.xls' for x in table_revenu]
+    _read_file_or_download(names, path_data, url_path)
+    data = None
+    for name in names:
+        path_file_on_disk = path_data + name
+        df = pd.read_excel(path_file_on_disk, sheetname=1, header=6)
+        assert all([x in df.columns for x in useless_cols])
+        assert len(df) == df.IRIS.nunique()
+        print("\t il y a ", len(df),
+          " iris différentes pour et ",
+          len(df.columns) - len(useless_cols), " features")
+        if data is None:
+            data = df
+        else:
+            data = data.merge(df, how='outer')
+            assert len(data) == len(df)
+    return data.rename(columns={'IRIS':'CODGEO'})
 
-## Revenu par personne
-revenu_personne = pd.read_excel(path_data+'RFDP2011IRI.xls', sheetname=1) #using int cause name of sheetname have some "é"
-# creating header from file
-header = revenu_personne.loc[5].tolist()
-revenu_personne.columns = header
-revenu_personne.rename(columns={'IRIS':'CODGEO'}, inplace=True)
-# to get real values
-revenu_personne = revenu_personne[6:]
-# creating new feature : sum of all feature
-features = [x for x in header if x not in ['IRIS','LIBIRIS','COM','LIBCOM','REG','DEP','ARR','CV','ZE2010']] # special list for this file
-# No need to sum features here (% and quantile)
-features.append('CODGEO')
-print("il y a  %d iris différentes pour le revenu par personne et %d features" % (len(revenu_personne.CODGEO.unique()), len(features) - 1))
-
-data = pd.merge(data, revenu_personne[features], on='CODGEO', how='outer')
+revenu = routine2(2011)
 
 
-## Revenu par unité de consomation
-revenu_uc = pd.read_excel(path_data+'RFDU2011IRI.xls', sheetname=1) #using int cause name of sheetname have some "é"
-# creating header from file
-header = revenu_uc.loc[5].tolist()
-revenu_uc.columns = header
-revenu_uc.rename(columns={'IRIS':'CODGEO'}, inplace=True)
-# to get real values
-revenu_uc = revenu_uc[6:]
-# creating new feature : sum of all feature
-features = [x for x in header if x not in ['IRIS','LIBIRIS','COM','LIBCOM','REG','DEP','ARR','CV','ZE2010']] # special list for this file
-# No need to sum features here (% and quantile)
-features.append('CODGEO')
-print("il y a  %d iris différentes pour le revenu par unité de consomation et %d features" % (len(revenu_uc.CODGEO.unique()), len(features) - 1))
 
-data = pd.merge(data, revenu_uc[features], on='CODGEO', how='outer')
-
-## Revenu % imposé + détails (% ménage imposé, dont traitement salaire etc..)
-revenu_impose = pd.read_excel(path_data+'RFST2011IRI.xls', sheetname=1) #using int cause name of sheetname have some "é"
-# creating header from file
-header = revenu_impose.loc[5].tolist()
-revenu_impose.columns = header
-revenu_impose.rename(columns={'IRIS':'CODGEO'}, inplace=True)
-# to get real values
-revenu_impose = revenu_impose[6:]
-# creating new feature : sum of all feature
-features = [x for x in header if x not in ['IRIS','LIBIRIS','COM','LIBCOM','REG','DEP','ARR','CV','ZE2010']] # special list for this file
-# No need to sum features here (% and quantile)
-features.append('CODGEO')
-print("il y a  %d iris différentes pour le revenu par ménage imposé et %d features" % (len(revenu_impose.CODGEO.unique()), len(features) - 1))
-
-data = pd.merge(data, revenu_impose[features], on='CODGEO', how='outer')
-
+data = equip_data.merge(revenu, how='outer')
 ### Fin de revenu
 #-------------------------------------------------------------------------
 
